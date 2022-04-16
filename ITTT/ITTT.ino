f0598f1bf68524ab5be229ee14a21896df766737
Michael_vd_Sanden <michael.mvdsanden@gmail.com>
2022-04-16 13:59:04 +0200

    Added main functionality

--- ITTT/ITTT.ino
+++ ITTT/ITTT.ino
@@ -1,44 +1,102 @@
 #include <Servo.h>
-#include <NewPing.h>
 
-const int trigPin = 11;
-const int echoPin = 10;
+const int trigPin = 5;
+const int sensorPin = A0;
+int sensorValue = 0;
 int distance;
 int distance2;
-int marginDistance = 2;
+int marginDistance = 5;
 Servo headServo;
-NewPing sonar(trigPin, echoPin);
+Servo armServo;
+Servo pitchServo;
+
+int down = 0;
 
 void setup() {
   Serial.begin(9600);
-  headServo.attach(12);
-  distance = sonar.ping_cm();
+  headServo.attach(13);
+  armServo.attach(12);
+  pitchServo.attach(8);
+  pinMode(trigPin, OUTPUT);                   // A low pull on pin COMP/TRIG
+  digitalWrite(trigPin, HIGH);                // Set to HIGH
+  delay(500);
+  Serial.println("Init the sensor");
+  distance = readDistance();
   distance2 = distance;
+
+  armServo.write(down);
 }
 
 void loop() {
-  // rotates the servo motor from 15 to 165 degrees
-  for (int i = 15; i <= 165; i++) {
+  for (int t = 0; t <= 1; t++)
+  {
+    eyesOpen();
+  }
+  eyesClosed();
+}
+void eyesOpen()
+{
+  // rotates the servo motor from 0 to 180 degrees
+  for (int i = 0; i <= 180; i++) {
     headServo.write(i);
     delay(10);
-    distance = sonar.ping_cm();
-    if (distance - distance2 > marginDistance || distance2 - distance > marginDistance)
+    if (i == 0 || i == 20 || i == 40 || i == 60 || i == 80 || i == 100 || i == 120 || i == 140 || i == 160 || i == 180)
     {
-      Serial.print("MOVED AT ");
-      Serial.println(i);
+      distance = readDistance();
+      Serial.print(distance);
+      if (distance - distance2 > marginDistance || distance2 - distance > marginDistance)
+      {
+        Serial.print("MOVED AT ");
+        Serial.println(i);
+        triggeredGoblin(i);
+      }
     }
   }
 
-  // Repeats the previous lines from 165 to 15 degrees
-  for (int i = 165; i > 15; i--) {
+  // Repeats the previous lines from 180 to 0 degrees
+  for (int i = 180; i > 0; i--) {
     headServo.write(i);
     delay(10);
-    distance2 = sonar.ping_cm();
-    if (distance - distance2 > marginDistance || distance2 - distance > marginDistance)
+    if (i == 0 || i == 20 || i == 40 || i == 60 || i == 80 || i == 100 || i == 120 || i == 140 || i == 160 || i == 180)
     {
-      Serial.print("MOVED AT ");
-      Serial.println(i);
+      distance2 = readDistance();
+      Serial.print(distance);
+      if (distance - distance2 > marginDistance || distance2 - distance > marginDistance)
+      {
+        Serial.print("MOVED AT ");
+        Serial.println(i);
+        triggeredGoblin(i);
+      }
     }
   }
+}
 
+void eyesClosed()
+{
+  Serial.println("eyes closed");
+  delay(3000);
+}
+
+int readDistance()
+{
+  //Serial.print("Distance=");
+  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, HIGH);
+  delay(20);
+  sensorValue = analogRead(sensorPin);
+
+  sensorValue = sensorValue * 1.1; //  (sensorValue * 5000 / 1024 ) / 4.125 = sensorValue * 1.1 , calculate the voltage value ADC collected and divide it by 4.125mV /cm.
+  //Serial.print(sensorValue);
+  //Serial.println("cm");
+  return (sensorValue);
+}
+
+int triggeredGoblin(int angle)
+{
+  int up = 90;
+  armServo.write(up);
+  pitchServo.write(angle);
+  Serial.println(angle);
+  delay(5000);
+  armServo.write(down);
 }
